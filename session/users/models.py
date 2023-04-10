d2c5194064e563747d8cd903a08aa6c902ecd463
Hongki <yunszone98@naver.com>
2023-04-10 18:45:36 +0900

    세션

--- session/users/models.py
+++ session/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # TODO: Profile 모델 생성
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    nickname = models.CharField(max_length=20, null=True)
+    image = models.ImageField(upload_to='profile/', null=True)
+
+    # 테이블 이름 설정
+    class Meta:
+        db_table = 'profile'
+
+    def __str__(self):
+        return self.nickname
